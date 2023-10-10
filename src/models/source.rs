3953d2d87708490fb5cb0994b596ef56457fb9c5
Alice Isabel <roridev@protonmail.com>
2023-10-10 14:51:45 -0300

    Add derives for `Source`

--- src/models/source.rs
+++ src/models/source.rs
@@ -1,14 +1,19 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, PartialOrd, PartialEq, Debug)]
 pub enum Source {
     Modrinth(ModrinthSource),
     Github(GithubSource),
     Https // Raw Https source. Requires TLS to work.
 }
 
+#[derive(Deserialize, Serialize, PartialOrd, PartialEq, Debug)]
 pub enum ModrinthSource {
     Mod,
     ModPack
 }
 
+#[derive(Deserialize, Serialize, PartialOrd, PartialEq, Debug)]
 pub enum GithubSource {
     Raw, // raw.github.com, used for getting config files and other things.
     Release // A github release
